64ae300c970d0c7d727c7a3e18670e2abcaae4ec
alebalcazarr <157438503+alebalcazarr@users.noreply.github.com>
2024-01-22 21:02:55 -0500

    Add files via upload

---
+++ Balcazar_alejandro_proyect1.py
@@ -0,0 +1,8 @@
+Python 3.11.2 (v3.11.2:878ead1ac1, Feb  7 2023, 10:02:41) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> # Creating a project for GitHub
+... # By Alejandro Balcazar
+...  
+... print("COP2002 Fall 2024")
+COP2002 Fall 2024
